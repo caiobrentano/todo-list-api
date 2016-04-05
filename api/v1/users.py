eedfced9732eed2ba04d373943197f321aed9b18
Caio Brentano dos Passos <caiobrentano@gmail.com>
2016-04-04 21:01:53 -0300

    ipdb removed

--- api/v1/users.py
+++ api/v1/users.py
@@ -11,13 +11,12 @@ def healthcheck():
 def create_user():
     username = request.json.get('username')
     password = request.json.get('password')
+
     if username is None or password is None:
         abort(400) # missing arguments
     if User.query.filter_by(username = username).first() is not None:
         abort(400) # existing user
 
-    import ipdb; ipdb.set_trace()
-    
     user = User(username = username)
     user.hash_password(password)
     db.session.add(user)
